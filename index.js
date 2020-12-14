3a808c87eda48718a5d85b1a120e22881eb4caf2
swagatikapanda123 <swagatikapanda164@gmail.com>
2020-12-15 01:27:31 +0530

    changing something

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hellow!')
+    res.send('hello!')
 })
 
 const PORT = process.env.PORT || 5000;
