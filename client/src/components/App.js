13a81b29cf9f47aaeded70eb6f5fce501ddfa8f3
swagatikapanda123 <swagatikapanda164@gmail.com>
2020-12-29 15:47:52 +0530

    posting surveys

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,9 +5,8 @@ import * as actions from '../actions';
 
 import Header from './Header';
 import Landing from './Landing';
-
-const Dashboard = () => <h2>dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 
 
 class App extends Component {
@@ -17,9 +16,9 @@ class App extends Component {
 
     render() {
     return (
-        <div className="container">
+        <div>
             <BrowserRouter>
-              <div>
+              <div className="container">
                   <Header />
                   <Route exact path="/" component={Landing} />
                   <Route exact path="/surveys" component={Dashboard} />
