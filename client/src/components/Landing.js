7bc8603c987b1dbfea168ad95678e554b92027d1
swagatikapanda123 <swagatikapanda164@gmail.com>
2021-02-04 05:26:06 +0530

    modifying landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Landing.css';
 
 const Landing = () => {
   return (
@@ -6,7 +8,13 @@ const Landing = () => {
       <h1>
         Maillit!
       </h1>
-      Collect feedback form your users
+     <h3> Collect feedback form your users! </h3>
+      <div className="action">
+      <Link to="/surveys" className="survey-link">
+          {/* <i className="material-icons">eco</i> */}
+         Get started!
+        </Link>
+       </div>
     </div>
   );
 };
